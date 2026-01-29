691fd82662b53c150336afed171997016193305f
Michael Herzog <mjherzog@nexb.com>
2026-01-28 17:14:33 -0800

    Fix typo in sidebars.js
    Signed-off-by: Michael Herzog <mjherzog@nexb.com>

--- website/sidebars.js
+++ website/sidebars.js
@@ -17,7 +17,7 @@ const sidebars = {
         'getting_started/getting_started-software-identification',
         'getting_started/getting_started-security',
         'getting_started/getting_started-compliance',
-        'getting-started/fetting-started-cravex',
+        'getting-started/getting-started-cravex',
     ],
 };
 
